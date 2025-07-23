0a30cc874bbcdf0d75052fbe8ce46873d07d9a14
Vishal <statusb130@gmail.com>
2025-07-23 18:25:10 +0530

    Dev

--- app/src/main/java/com/rach/habitchange/utils/Secrets.kt
+++ app/src/main/java/com/rach/habitchange/utils/Secrets.kt
@@ -3,7 +3,7 @@ package com.rach.habitchange.utils
 import com.rach.habitchange.BuildConfig
 
 object Secrets {
-    //AD Unit Id of ADmob
+    //okay
     const val ADS_UNIT_ID = BuildConfig.BANNER_ADS_UNIT_ID
 }
 
